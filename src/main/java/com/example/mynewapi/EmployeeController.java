60e7322ff090180e5b4e4f29ec04bd6fab2facb1
XieChen-GB <xieche@gmail.com>
2026-05-31 09:41:59 +0900

    修改EmployeeController.java文件名拼写错误

--- src/main/java/com/example/mynewapi/EmployeeController.java
+++ src/main/java/com/example/mynewapi/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class EmpoyeeController {
+public class EmployeeController {
 
     @GetMapping("/hello")
     public String hello() {
